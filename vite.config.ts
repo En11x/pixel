a601bfd988bcbbea69bc23025d785916197d0aa1
Xin <497421855@qq.com>
2023-05-20 10:20:17 +0800

    feat: add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+import presetAttributify from '@unocss/preset-attributify'
+import presetUno from '@unocss/preset-uno'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    UnoCSS({
+      presets: [presetUno(), presetAttributify()],
+    }),
+    react(),
+  ],
 })
